c5ce3407183ac8c1a62e9556bc3dad1bd76081d1
raji30 <rajalakshmi30@gmail.com>
2018-11-23 01:03:54 +0000

    tms web with sai changes
    tms web with sai changes

---
+++ coreui-pro-angular-admin-template-starter-2.0.0/src/app/app.routing.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Import Containers
+import { DefaultLayoutComponent } from './containers';
+import { LoginComponent } from './views/login/login.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: './views/dashboard/dashboard.module#DashboardModule'
+      },
+      // {
+      //   path: 'login',
+      //   loadChildren: './views/login/login.module#LoginModule'
+      // }
+   ]
+  },
+  { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
