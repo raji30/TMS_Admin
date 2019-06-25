70356d159562da647013b3b9352048d5e622479d
raji30 <rajalakshmi30@gmail.com>
2019-06-24 19:28:00 -0700

    Scheduling API added

---
+++ TMS.Api/Controllers/ScheduleController.cs
@@ -0,0 +1,42 @@
+﻿using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using TMS.BusinessObjects;
+using TMS.Data;
+
+namespace TMS.Api.Controllers
+{
+    public class ScheduleController : ApiController
+    {
+        // GET: Schedule
+        [HttpGet]
+        [Route("GetSchedule")]
+        [SwaggerOperation("GetSchedule")]
+        public HttpResponseMessage GetSchedule(string orderKey)
+        {
+            if (string.IsNullOrEmpty(orderKey))
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            SchedulingDL dl = new SchedulingDL();
+           var list= dl.GetSchedulingDetails(Guid.Parse(orderKey));
+            return Request.CreateResponse(HttpStatusCode.OK, list, Configuration.Formatters.JsonFormatter);
+
+        }
+
+        [HttpPost]
+        [Route("PostSchedule")]
+        [SwaggerOperation("PostSchedule")]
+        public HttpResponseMessage PostSchedule(ScheduleOrderBO orderBO)
+        {
+           
+            SchedulingDL dl = new SchedulingDL();
+            var routeKey = dl.InsertSchedule(orderBO);
+            return Request.CreateResponse(HttpStatusCode.OK, routeKey, Configuration.Formatters.JsonFormatter);
+
+        }
+    }
+}
