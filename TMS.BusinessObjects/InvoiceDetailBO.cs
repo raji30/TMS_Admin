f9379dc542121ae3118cd9e18423395345754e8e
raji30 <rajalakshmi30@gmail.com>
2019-08-25 22:28:53 -0700

    Invoice BO

---
+++ TMS.BusinessObjects/InvoiceDetailBO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.BusinessObjects
+{
+   public class InvoiceDetailBO
+    {
+        public Guid Itemkey { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public short ItemType { get; set; }
+        public decimal Quantity { get; set; }
+        public string InvoiceDescription { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal ExcessAmount { get; set; }
+        public Guid InvoiceLineKey { get; set; }
+        public Guid InvoiceKey { get; set; }
+    }
+}
